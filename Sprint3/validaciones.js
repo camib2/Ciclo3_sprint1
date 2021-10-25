e89ece052e94979ec339662c041fa2d7ceddd761
Camila <cbenitezc@unal.edu.co>
2021-10-24 23:26:24 -0500

    Sprint 3 completo pero 0/10 calificacion

--- Sprint3/validaciones.js
+++ Sprint3/validaciones.js
@@ -2,11 +2,10 @@ const string =document.querySelector("#in_nombre_usuario");
 const stringA =document.querySelector("#in_contrasena");
 const stringB =document.querySelector("#in_confirmar_contrasena");
 const boton_registro =document.querySelector("#btn_registrar_usuario");
-const espacioMensaje=document.querySelector(".mensaje_error")
-
-
+//const espacioMensaje=document.querySelector(".mensaje_error")
 
 let registros =[];
+
 boton_registro.addEventListener("click", agregarRegistro);
 
 class MiConstructor{
@@ -26,43 +25,65 @@ function agregarRegistro(){
 
     if(validar_nombre_usuario(usuarioCreado.usuario)){
         if(validar_contrasena(usuarioCreado.contrasena)){
-                if(usuarioCreado.contrasena===usuarioCreado.confirmacion){
-                    console.log("Se registro el usuario");
-                }else{console.log("La confirmacion no coincide con la contraseña");
+                if(confirmar_contrasena(usuarioCreado.contrasena,usuarioCreado.confirmacion)){
+                    //console.log("Se registro el usuario");
+                    registros.push(usuarioCreado);
+                    OrdenarArreglo(registros);
+                    console.log(registros);
+                }else{//console.log("La confirmacion no coincide con la contraseña");
                 }
-        }else{console.log("La contraseña no es apta");
+        }else{//console.log("La contraseña no es apta");
         }
-    }else{console.log("El usuario no es apto");
+    }else{//console.log("El usuario no es apto");
     }
 
     string.value="";
     stringA.value="";
     stringB.value="";
-}
 
-function validar_nombre_usuario(string){
-    // La ñ arroja falso no esta incluida
-    patron2 = /^[a-zA-Z]{6,30}$/g;
-    if(patron2.test(string)){
-        return true;
-    }else{
-        return false;
-    }
-}
 
-function validar_contrasena (string){
-    // 6, minmo 6 sin maximo
-    patron1 = /^[A-Z0-9]{6,}$/g;
-    if(patron1.test(string)){
-        return true;
-    }else{
+    function validar_nombre_usuario(string){
+        // La ñ arroja falso no esta incluida
+        patron2 = /^[a-zA-Z]{6,30}$/g;
+        if(patron2.test(string)){
+            return true;
+        }else{
             return false;
+        }
     }
+
+    function validar_contrasena (string){
+        // 6, minmo 6 sin maximo
+        patron1 = /^[A-Z0-9]{6,}$/g;
+        if(patron1.test(string)){
+            return true;
+        }else{
+                return false;
+        }
+    }
+
+    function confirmar_contrasena(string1,string2){
+        if(string1===string2){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+function OrdenarArreglo(arreglo){
+    arreglo.sort((a,b) =>{
+        if(a.usuario<b.usuario){
+            return -1;
+        }else if(a.usuario>b.usuario){
+            return 1;
+        }else{
+            return 0;
+        }
+    });
 }
 
 /*
-function OrdenarArreglo(arreglo){}
-
 module.exports.validar_nombre_usuario = validar_nombre_usuario;
 module.exports.validar_contrasena=validar_contrasena;
 module.exports.confirmar_contrasena=confirmar_contrasena;
